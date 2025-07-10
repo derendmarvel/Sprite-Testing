170cdbf510bff495beb8ecabf431b702e8863377
Syamsuddin Putra Riefli <53290732+MYSTELTAiNN-ID@users.noreply.github.com>
2025-07-10 11:50:31 +0700

    Generating World (with GameplayKit)

--- Sprite Testing/GameScene.swift
+++ Sprite Testing/GameScene.swift
@@ -8,10 +8,60 @@
 import SpriteKit
 import GameplayKit
 
+enum ChunkType: CaseIterable {
+    case barrier, gerobak1, gerobak2
+
+    var weight: Int {
+        switch self {
+        case .barrier: return 33
+        case .gerobak1: return 33
+        case .gerobak2: return 33
+        }
+    }
+
+    func createNode(at position: CGPoint) -> SKNode {
+        let object: SKSpriteNode
+        switch self {
+        case .barrier:
+            object = SKSpriteNode(imageNamed: "barrier")
+            object.setScale(0.3)
+        case .gerobak1:
+            object = SKSpriteNode(imageNamed: "gerobak-1")
+            object.setScale(0.3)
+        case .gerobak2:
+            object = SKSpriteNode(imageNamed: "gerobak-2")
+            object.setScale(0.3)
+        }
+
+        object.position = position
+        object.zPosition = 1
+        let scaledSize = CGSize(
+            width: object.size.width * object.xScale,
+            height: object.size.height * object.yScale
+        )
+        object.physicsBody = SKPhysicsBody(rectangleOf: scaledSize)
+        object.physicsBody?.isDynamic = false
+        object.physicsBody?.categoryBitMask = 0x1 << 2
+        object.physicsBody?.contactTestBitMask = 0x1 << 0
+        object.physicsBody?.collisionBitMask = 0x1 << 0
+        return object
+    }
+}
+
+
 class GameScene: SKScene, SKPhysicsContactDelegate{
     var character: SKSpriteNode!
     var backgrounds: [SKSpriteNode] = []
     var objectUnderCharacter: SKSpriteNode?
+    
+    //Noise
+    var noiseMap: GKNoiseMap!
+    var lastGeneratedX: CGFloat = 800
+    let chunkSpacing: CGFloat = 300
+    var scrollOffsetX: CGFloat = 0
+    var weightedChunkTypes: [ChunkType] = []
+    var recentChunks: [ChunkType] = []
+
     
     override func didMove(to view: SKView) {
         physicsWorld.gravity = CGVector(dx: 0, dy: -5)
@@ -28,6 +78,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             addChild(background)
         }
         
+        let noiseSource = GKPerlinNoiseSource(frequency: 0.04, octaveCount: 3, persistence: 0.5, lacunarity: 2, seed: Int32(Int.random(in: 0..<2147483648)))
+        let noise = GKNoise(noiseSource)
+        noiseMap = GKNoiseMap(
+            noise,
+            size: vector_double2(1000, 1),
+            origin: vector_double2(0, 0),
+            sampleCount: vector_int2(1000, 1),
+            seamless: false
+        )
+        
+        for type in ChunkType.allCases {
+            weightedChunkTypes += Array(repeating: type, count: type.weight)
+        }
+
+        
         let ground = SKNode()
         ground.position = CGPoint(x: 0, y: self.size.height / 4 - 75)
         ground.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x:0, y:0), to: CGPoint(x: self.size.width, y: 0))
@@ -35,11 +100,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         ground.physicsBody?.categoryBitMask = 0x1 << 1
         addChild(ground)
         
-        let spawnAction = SKAction.sequence([
-            SKAction.run { self.spawnGroundObject() },
-            SKAction.wait(forDuration: 2.0)
-        ])
-        run(SKAction.repeatForever(spawnAction))
+//        let spawnAction = SKAction.sequence([
+//            SKAction.run { self.spawnGroundObject() },
+//            SKAction.wait(forDuration: 2.0)
+//        ])
+//        run(SKAction.repeatForever(spawnAction))
         
         animateCharacter()
         
@@ -133,6 +198,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
                 bg.position.x += bg.size.width * 2
             }
         }
+        
+        let scrollSpeed: CGFloat = 2
+        scrollOffsetX += scrollSpeed
+        generateProceduralChunks(cameraX: scrollOffsetX)
+//        generateProceduralChunks(cameraX: character.position.x)
 
         // Move character with object if standing on it
         if let object = objectUnderCharacter {
@@ -158,55 +228,132 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         }
     }
     
-    func spawnGroundObject() {
-        let imageNames = ["barrier", "gerobak-1", "gerobak-2"]
-        let randomImageName = imageNames.randomElement() ?? "barrier"
-        
-        let object = SKSpriteNode(imageNamed: randomImageName)
+    func generateProceduralChunks(cameraX: CGFloat) {
+        let spawnAheadDistance: CGFloat = 300
 
-        // Default values
-        var scale: CGFloat = 0.5
-        var yPos: CGFloat = self.size.height / 3
+        while lastGeneratedX < cameraX + spawnAheadDistance {
+            // 1. Sample noise
+            let noiseIndex = Int32(lastGeneratedX / 200)
+            let noiseValue = noiseMap.value(at: vector_int2(noiseIndex, 0))
+            let normalized = max(0, min((noiseValue + 1) / 2, 1)) // 0...1
 
-        // Customize size and position per image
-        switch randomImageName {
-        case "barrier":
-            scale = 0.3
-            yPos = self.size.height / 3.5
-        case "gerobak-1":
-            scale = 0.5
-            yPos = self.size.height / 3
-        case "gerobak-2":
-            scale = 0.4
-            yPos = self.size.height / 3.5
-        default:
-            break
+            // 2. Use noise as seed to shuffle weighted chunk types
+            var shuffled = weightedChunkTypes.shuffled()
+            let noiseSeedIndex = Int(CGFloat(normalized) * CGFloat(shuffled.count - 1))
+            let candidate = shuffled[noiseSeedIndex]
+
+            // 3. Check if we've seen this chunk type too recently
+            var type = candidate
+            if recentChunks.suffix(3).allSatisfy({ $0 == candidate }) {
+                // Force something else if it's too repetitive
+                type = ChunkType.allCases.filter { $0 != candidate }.randomElement()!
+            }
+
+            recentChunks.append(type)
+            if recentChunks.count > 5 {
+                recentChunks.removeFirst()
+            }
+
+            // 4. Spawn the chunk
+            let position = CGPoint(x: lastGeneratedX, y: self.size.height / 3 - 40)
+            let chunk = type.createNode(at: position)
+            addChild(chunk)
+
+            let scrollSpeed: CGFloat = 240.0 // pts/sec
+            let travelDistance = position.x + 300
+            let moveDuration = TimeInterval(travelDistance / scrollSpeed)
+
+            chunk.run(.sequence([.moveBy(x: -travelDistance, y: 0, duration: moveDuration), .removeFromParent()]))
+
+            lastGeneratedX += CGFloat.random(in: 150...300)
+            print("Generated \(type) at x=\(lastGeneratedX)")
         }
-
-        object.setScale(scale)
-        object.position = CGPoint(x: self.size.width + object.size.width / 2, y: yPos)
-        object.zPosition = 1
-
-        // Use actual scaled size for physics body
-        let scaledSize = CGSize(width: object.size.width * object.xScale,
-                                height: object.size.height * object.yScale)
-
-        object.physicsBody = SKPhysicsBody(rectangleOf: scaledSize)
-        object.physicsBody?.isDynamic = false
-        object.physicsBody?.categoryBitMask = 0x1 << 2 // Obstacle
-        object.physicsBody?.contactTestBitMask = 0x1 << 0
-        object.physicsBody?.collisionBitMask = 0x1 << 0
-
-        addChild(object)
-
-        let moveDistance = self.size.width + 60 + object.size.width
-        let backgroundSpeed: CGFloat = 240 // pts/sec
-        let moveDuration = moveDistance / backgroundSpeed
-
-        let move = SKAction.moveBy(x: -moveDistance, y: 0, duration: TimeInterval(moveDuration))
-        let remove = SKAction.removeFromParent()
-        object.run(SKAction.sequence([move, remove]))
     }
+
+    
+//    func generateProceduralChunks(cameraX: CGFloat) {
+//        let spawnAheadDistance: CGFloat = 300
+//
+//        while lastGeneratedX < cameraX + spawnAheadDistance {
+//            // Use noise to get a smooth value
+//            let noiseIndex = Int32(lastGeneratedX / CGFloat.random(in: 150...300))
+//            let noiseValue = noiseMap.value(at: vector_int2(noiseIndex, 0))
+//            let normalized = max(0, min((noiseValue + 1) / 2, 1)) // -1 to 1 → 0 to 1
+//
+//            // Select type based on weighted distribution
+//            let weightedIndex = Int(CGFloat(normalized) * CGFloat(weightedChunkTypes.count - 1))
+//            let type = weightedChunkTypes[weightedIndex]
+//
+//            // Place chunk
+//            let position = CGPoint(x: lastGeneratedX, y: self.size.height / 3 - 40)
+//            let chunk = type.createNode(at: position)
+//            addChild(chunk)
+//
+//            // Animate chunk moving left at the same speed as the background
+//            let scrollSpeed: CGFloat = 240.0 // pts per second
+//            let travelDistance = position.x + 300
+//            let moveDuration = TimeInterval(travelDistance / scrollSpeed)
+//
+//            let move = SKAction.moveBy(x: -travelDistance, y: 0, duration: moveDuration)
+//            let remove = SKAction.removeFromParent()
+//            chunk.run(SKAction.sequence([move, remove]))
+//
+//            // Random spacing between chunks
+//            lastGeneratedX += CGFloat.random(in: 150...300)
+//
+//            print("Generated \(type) at x=\(lastGeneratedX)")
+//        }
+//    }
+    
+//    func spawnGroundObject() {
+//        let imageNames = ["barrier", "gerobak-1", "gerobak-2"]
+//        let randomImageName = imageNames.randomElement() ?? "barrier"
+//        
+//        let object = SKSpriteNode(imageNamed: randomImageName)
+//
+//        // Default values
+//        var scale: CGFloat = 0.5
+//        var yPos: CGFloat = self.size.height / 3
+//
+//        // Customize size and position per image
+//        switch randomImageName {
+//        case "barrier":
+//            scale = 0.3
+//            yPos = self.size.height / 3.5
+//        case "gerobak-1":
+//            scale = 0.5
+//            yPos = self.size.height / 3
+//        case "gerobak-2":
+//            scale = 0.4
+//            yPos = self.size.height / 3.5
+//        default:
+//            break
+//        }
+//
+//        object.setScale(scale)
+//        object.position = CGPoint(x: self.size.width + object.size.width / 2, y: yPos)
+//        object.zPosition = 1
+//
+//        // Use actual scaled size for physics body
+//        let scaledSize = CGSize(width: object.size.width * object.xScale,
+//                                height: object.size.height * object.yScale)
+//
+//        object.physicsBody = SKPhysicsBody(rectangleOf: scaledSize)
+//        object.physicsBody?.isDynamic = false
+//        object.physicsBody?.categoryBitMask = 0x1 << 2 // Obstacle
+//        object.physicsBody?.contactTestBitMask = 0x1 << 0
+//        object.physicsBody?.collisionBitMask = 0x1 << 0
+//
+//        addChild(object)
+//
+//        let moveDistance = self.size.width + 60 + object.size.width
+//        let backgroundSpeed: CGFloat = 240 // pts/sec
+//        let moveDuration = moveDistance / backgroundSpeed
+//
+//        let move = SKAction.moveBy(x: -moveDistance, y: 0, duration: TimeInterval(moveDuration))
+//        let remove = SKAction.removeFromParent()
+//        object.run(SKAction.sequence([move, remove]))
+//    }
     
     func didBegin(_ contact: SKPhysicsContact) {
         let a = contact.bodyA
